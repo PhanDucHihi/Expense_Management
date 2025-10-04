e3cb0c9e7a549fa4d8fac028e5acb8c6e181ce1e
phanduc0136@gmail.com <phanduc0136@gmail.com>
2025-10-04 11:25:30 +0700

    use Suspense

--- fe/my-app/src/app/(chatApp)/budget/page.tsx
+++ fe/my-app/src/app/(chatApp)/budget/page.tsx
@@ -2,12 +2,15 @@
 
 import Header, { ActionType } from "../header";
 import BudgetDashBoard from "./budgetDashboard";
+import { Suspense } from "react";
 
 export default function BudgetPage() {
   return (
     <div className="p-5 space-y-5">
       <Header title="Thêm mới ngân sách" actionType={ActionType.BUDGET} />
-      <BudgetDashBoard />
+      <Suspense fallback={<div>Loading dashboard...</div>}>
+        <BudgetDashBoard />
+      </Suspense>
     </div>
   );
 }
