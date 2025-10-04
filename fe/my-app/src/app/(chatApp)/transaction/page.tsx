8004e7b6a412cc8a8ccbab57a657bda6ade0ce1a
phanduc0136@gmail.com <phanduc0136@gmail.com>
2025-10-04 11:35:48 +0700

    fix

--- fe/my-app/src/app/(chatApp)/transaction/page.tsx
+++ fe/my-app/src/app/(chatApp)/transaction/page.tsx
@@ -1,26 +1,7 @@
-"use client";
+export const dynamic = "force-dynamic";
 
-import TransactionTale from "./(table)/page";
-import Header, { ActionType } from "../header";
-import { useWalletStore } from "@/store/walletStore";
-import { WalletType } from "@/types/wallet";
-import { SavingProgressCard } from "./SavingProgressCard";
+import Transaction from "./transaction";
 
-export default function Transaction() {
-  const currentWallet = useWalletStore((state) => state.currentWallet);
-
-  return (
-    <div className="p-5 space-y-5">
-      <Header title="Thêm mới giao dịch" actionType={ActionType.TRANSACTION} />
-
-      {currentWallet?.type === WalletType.GOAL && (
-        <SavingProgressCard
-          balance={currentWallet.balance}
-          targetAmount={currentWallet.targetAmount || 0}
-          deadline={currentWallet.deadline}
-        />
-      )}
-      <TransactionTale />
-    </div>
-  );
+export default function TransactionPage() {
+  return <Transaction />;
 }
