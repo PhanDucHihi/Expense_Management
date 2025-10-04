5d399283c21d457fa5eba39da176ad200b944183
phanduc0136@gmail.com <phanduc0136@gmail.com>
2025-10-04 09:39:11 +0700

    change main.ts

--- be/src/main.ts
+++ be/src/main.ts
@@ -19,6 +19,6 @@ async function bootstrap() {
   app.useGlobalFilters(new CatchEverythingFilter(httpAdapterHost));
   app.setGlobalPrefix('api');
   app.use(cookieParser());
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
 }
 bootstrap();
